9ba4a0276f695c87dc87e411b43c6b4c7e7ac004
fawzyebrahim351 <fawzyebrahim777@gmail.com>
2023-09-11 06:56:41 +0200

    rating app design from frontend mentor

---
+++ javascript/main.js
@@ -0,0 +1,30 @@
+let lis = document.querySelectorAll('ul li');
+let button = document.querySelector('button');
+let rate = document.querySelector('.rate');
+let thank = document.querySelector('.thank');
+let yourRate = document.querySelector('.your-rate')
+
+lis.forEach(function(e,no,list){
+    e.onclick = function(){
+        // remove checked class from lis 
+        lis.forEach(e => {
+            e.classList.remove('checked');
+        })
+        // add class to active one 
+        this.classList.add('checked')
+    }
+})
+button.onclick = function(){
+    // move boxes 
+    rate.style.left = '-380px'
+    thank.style.left = '-380px'
+    // get data 
+    let checkEle = 0;
+    lis.forEach(element => {
+        if(element.classList.contains('checked')){
+            checkEle = element.innerHTML
+        }
+    });
+    yourRate.innerHTML = `Your rate is ${checkEle} out of 5`
+
+}
